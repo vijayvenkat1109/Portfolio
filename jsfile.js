0c7a043c01f40fca642e885bf1dacb8312ec1450
VIJAYVENKATESHWARAN M <vijayvenkatm05@gmail.com>
2024-09-27 00:36:30 +0530

    Portfolio files

---
+++ jsfile.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector(".Menu");
+var open=document.getElementById("side");
+var closee=document.getElementById("xm");
+menu.addEventListener("click",()=>{
+    open.style.display="block";
+   
+})
+closee.addEventListener("click",()=>{
+    open.style.display="none";
+   
+})
